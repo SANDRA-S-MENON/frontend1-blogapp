7904fac50b76769eebdca45c18959b5cd2b63800
SANDRA-S-MENON <sandramenon2002@gmail.com>
2024-07-28 21:05:53 +0530

    viewmypost completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+    const navigate=useNavigate()
+    const logout=()=>{
+        sessionStorage.clear()
+        navigate("/")
+    }
   return (
     <div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">BlogApp</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -18,9 +24,11 @@ const Navbar = () => {
           <a class="nav-link" href="/viewall">viewall</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">view my post</a>
+          <a class="nav-link" href="/viewmypost">view my post</a>
         </li>
-        
+        <li class="nav-item">
+          <button onClick={logout} className="btn btn-success">log out</button>
+        </li>
       </ul>
     </div>
   </div>
